80ae6f207c6eddfaeae815e3ad413b1abf94cfdc
Brett Nicholas <brett.r.nicholas.15@dartmouth.edu>
2017-08-21 20:55:30 -0400

    added comments

--- src/wsrsa2048.cpp
+++ src/wsrsa2048.cpp
@@ -1,17 +1,18 @@
 #include "wsrsa2048.hpp"
 
-void wsrsa1024( memword_t privexp[NUM_MEMWORDS],  // private exponent
-				RSAmode_t mode,
+void wsrsa1024( memword_t privexp[NUM_MEMWORDS],    // BRAM holding private exponent
+				RSAmode_t mode,     // mode: encrypt, decrypt, or load private exponent from BRAM
 				uintRSA_t base,     // base (plain/cipher)text
 				uintRSA_t publexp,  // public exponent
-				uintRSA_t modulus,  // shared modulus
-				uintRSA_t *result )
+				uintRSA_t modulus,  // modulus
+				uintRSA_t *result ) // result
 {
 
 	static uintRSA_t priv=0;
 
 	switch(mode)
-	{
+	{	
+	// Load the private key (from BRAM) into local ap_uint<1024> variable.
 	case DECRYPTKEYINIT:
 		cout << "mode = DECRYPTKEYINIT" << endl;
 		for (int i=0; i<NUM_MEMWORDS; i++)
@@ -23,11 +24,15 @@ void wsrsa1024( memword_t privexp[NUM_MEMWORDS],  // private exponent
 		*result = 0;
 		break;
 
+	// Encrypts data using RSA modular exponentiation: result = base^(private exponent) % modulus
+	// base is the plaintext, exponent is private exponent, modulus is the shared modulus
 	case ENCRYPT:
 		cout << "mode = ENCRYPT" << endl;
 		rsaModExp(base,publexp,modulus,result);
 		break;
 
+	// Decrypts data using RSA modular exponentiation: result = base^(public exponent) % modulus
+	// base is the ciphertext, exponent is private exponent, modulus is the shared modulus	case DECRYPT:
 	case DECRYPT:
 	default:
 		cout << "mode = DECRYPT" << endl;
